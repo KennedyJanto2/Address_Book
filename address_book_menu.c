30deb9e3253bf8476234165694427151c802cea8
William Go <wlgo@cpp.edu>
2022-03-26 14:30:47 -0700

    Working on edit_contact

---
+++ address_book_menu.c
@@ -0,0 +1,221 @@
+#include <stdio.h>
+//#include <stdio_ext.h> its pulling an error so im just commenting it out for now
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "address_book_fops.h"
+//#include "abk_log.h" doesnt exist
+#include "address_book_menu.h"
+#include "address_book.h"
+
+int get_option(int type, const char *msg)
+{
+	/*
+	 * Mutilfuction user intractions like
+	 * Just an enter key detection
+	 * Read an number
+	 * Read a charcter
+	 */ 
+
+	/* Fill the code to add above functionality */
+}
+
+Status save_prompt(AddressBook *address_book)
+{
+	char option;
+
+	do
+	{
+		main_menu();
+
+		option = get_option(CHAR, "\rEnter 'N' to Ignore and 'Y' to Save: ");
+
+		if (option == 'Y')
+		{
+			save_file(address_book);
+			printf("Exiting. Data saved in %s\n", DEFAULT_FILE);
+
+			break;
+		}
+	} while (option != 'N');
+
+	free(address_book->list);
+
+	return e_success;
+}
+
+Status list_contacts(AddressBook *address_book, const char *title, int *index, const char *msg, Modes mode)
+{
+	/* 
+	 * Add code to list all the contacts availabe in address_book.csv file
+	 * Should be menu based
+	 * The menu provide navigation option if the entries increase the page size
+	 */ 
+
+	return e_success;
+}
+
+void menu_header(const char *str)
+{
+	fflush(stdout);
+
+	system("clear");
+
+	printf("#######  Address Book  #######\n");
+	if (*str != '\0')
+	{
+		printf("#######  %s\n", str);
+	}
+}
+
+void main_menu(void)
+{
+	menu_header("Features:\n");
+
+	printf("0. Exit\n");
+	printf("1. Add Contact\n");
+	printf("2. Search Contact\n");
+	printf("3. Edit Contact\n");
+	printf("4. Delete Contact\n");
+	printf("5. List Contacts\n");
+	printf("6. Save\n");
+	printf("\n");
+	printf("Please select an option: ");
+}
+
+Status menu(AddressBook *address_book)
+{
+	ContactInfo backup;
+	Status ret;
+	int option;
+
+	do
+	{
+		main_menu();
+
+		option = get_option(NUM, "");
+
+		if ((address_book-> count == 0) && (option != e_add_contact))
+		{
+			get_option(NONE, "No entries found!!. Would you like to add? Use Add Contacts");
+
+			continue;
+		}
+
+		switch (option)
+		{
+			case e_add_contact:
+				/* Add your implementation to call add_contacts function here */
+				break;
+			case e_search_contact:
+				search_contact(address_book);
+				break;
+			case e_edit_contact:
+				edit_contact(address_book);
+				break;
+			case e_delete_contact:
+				delete_contact(address_book);
+				break;
+			case e_list_contacts:
+				break;
+				/* Add your implementation to call list_contacts function here */
+			case e_save:
+				save_file(address_book);
+				break;
+			case e_exit:
+				break;
+		}
+	} while (option != e_exit);
+
+	return e_success;
+}
+
+Status add_contacts(AddressBook *address_book)
+{
+	/* Add the functionality for adding contacts here */
+}
+
+Status search(const char *str, AddressBook *address_book, int loop_count, const char *msg, Modes mode)
+{
+	/* Add the functionality for adding contacts here */
+}
+
+Status search_contact(AddressBook *address_book)
+{
+	/* Add the functionality for search contacts here */
+}
+
+Status edit_contact(AddressBook *address_book)
+{
+	int option;
+	char searchWord;
+
+	FILE *fp, *fp1;
+	fp = fopen("contact.txt", "r+:");
+	fp1 = fopen("temp.txt", "w");
+
+	menu_header("Search Contact to Edit by: \n");
+	printf("0. Back\n");
+	printf("1. Name\n");
+	printf("2. Phone Number\n");
+	printf("3. Email ID\n");
+	printf("4. Serial No.\n");
+	printf("\n");
+	printf("Please select an option: ");
+
+	do
+	{
+		option = get_option(NUM, "");
+		if((address_book-> count == 0) && (option != e_first_opt))
+		{
+			get_option(NONE, "No entries found!");
+		}
+
+		switch(option)
+		{
+			//Back
+			case e_back:
+			break;
+			//Name
+			case e_second_opt:
+				printf("Enter the Name: ");
+				scanf("%s", searchWord);
+				search(searchWord, address_book, address_book-> count, searchWord, e_edit);
+				//enter code here to edit by name
+			break;
+			//Phone No
+			case e_third_opt:
+				printf("Enter the Phone Number: ");
+				scanf("%s", searchWord);
+				search(searchWord, address_book, address_book-> count, searchWord, e_edit);
+				//enter code here to edit by phone
+			break;
+			//Email ID
+			case e_fourth_opt:
+				printf("Enter the Email ID: ");
+				scanf("%s", searchWord);
+				search(searchWord, address_book, address_book-> count, searchWord, e_edit);
+				//enter code here to edit by email
+			break;
+			//Serial No
+			case e_fifth_opt:
+				printf("Enter the Serial No: ");
+				scanf("%s", searchWord);
+				search(searchWord, address_book, address_book-> count, searchWord, e_edit);
+				//enter code here to edit by serial
+			break;
+			//No Option
+			case e_no_opt:
+			break;
+		}
+
+	} while (option != e_exit);
+	
+	return e_success;
+}
+
+Status delete_contact(AddressBook *address_book)
+{
+	/* Add the functionality for delete contacts here */
+}
